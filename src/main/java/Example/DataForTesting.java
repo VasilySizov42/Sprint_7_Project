00d486abb537d8746856c473a1d43167427dafec
Vasiliy Sizov <pinky-77@yandex.ru>
2023-09-29 14:04:50 +0300

    Edited CourierTest.java MethodFactory.java DataForTesting.java

--- src/main/java/Example/DataForTesting.java
+++ src/main/java/Example/DataForTesting.java
@@ -4,5 +4,7 @@ public class DataForTesting {
     public static final String YANDEX_SCOOTER = "https://qa-scooter.praktikum-services.ru";
     public static final String BASIC_HANDLE = "/api/v1/";
     public static final String ORDERS_HANDLE = "orders";
+    public static final String COURIER_HANDLE = "courier";
+    public static final String LOGIN_HANDLE = "/login";
 
 }
--- src/main/java/Example/MethodFactory.java
+++ src/main/java/Example/MethodFactory.java
@@ -2,53 +2,99 @@ package Example;
 
 import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.RandomStringUtils;
 
-import static Example.DataForTesting.BASIC_HANDLE;
-import static Example.DataForTesting.YANDEX_SCOOTER;
+import java.util.Map;
+
+import static Example.DataForTesting.*;
 import static io.restassured.RestAssured.given;
+import static java.lang.String.valueOf;
+import static java.net.HttpURLConnection.HTTP_CREATED;
+import static java.net.HttpURLConnection.HTTP_OK;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.is;
 
 public class MethodFactory {
-
-    @Step("checking the orders.id not null")
-    public static void checkOrdersIdNotNull(Response response) {
-        response.then().assertThat().body("orders[0].id", notNullValue());
-        //response.then().assertThat().body("orders[1].id", equalTo(37));
-        System.out.println(response.body().asString());
+    public static RequestSpecification scope() {
+        return  given().log().all()
+                .contentType(ContentType.JSON)
+                .baseUri(YANDEX_SCOOTER)
+                ;
     }
-
-    @Step ("checking the Status Code")
-    public static void checkForStatusCode(Response response, int status) {
-        response.then().statusCode(status);
-        System.out.println(response.statusCode());
-    }
-
     @Step ("getting Response via GET Request")
-    public static Response getResponseViaGet(String finalHandle) {
-        Response response = given().log().all()
-                .when().get(BASIC_HANDLE + finalHandle);
-        return response;
+    public static ValidatableResponse getResponseViaGet(String finalHandle) {
+        return scope()
+                .get(BASIC_HANDLE + finalHandle)
+                .then().log().all()
+                ;
     }
+    @Step ("checking the Status Code")
+    public static void checkForStatusCode(ValidatableResponse response, int status) {
+        response.statusCode(status)
+        ;
+    }
+    @Step("checking the orders.id not null")
+    public static void checkOrdersIdNotNull(ValidatableResponse response) {
+        response.assertThat()
+                .body("orders[0].id", notNullValue())
+        ;
+    }
+    @Step("courier data generation")
     public static Courier genericCourier() {
         return new Courier("Bazz"+ RandomStringUtils.randomAlphanumeric(3,5),
                 RandomStringUtils.randomAlphanumeric(7,10), "Bazz");
     }
-    public static Response loginCourier(Credentials creds) {
-        return given().log().all()
-                .contentType(ContentType.JSON)
-                .baseUri(YANDEX_SCOOTER)
-                .body(creds)
+    public static ValidatableResponse loginCourier(Credentials credentials) {
+        return scope()
+                .body(credentials)
                 .when()
-                .post(BASIC_HANDLE + "courier/login");
+                .post(BASIC_HANDLE + COURIER_HANDLE + LOGIN_HANDLE)
+                .then().log().all()
+                ;
     }
-    public static Response createCourier(Courier courier) {
-        return given().log().all()
-                .contentType(ContentType.JSON)
-                .baseUri(YANDEX_SCOOTER)
+    public static ValidatableResponse createCourier(Courier courier) {
+        return scope()
                 .body(courier)
                 .when()
-                .post(BASIC_HANDLE + "courier");
+                .post(BASIC_HANDLE + COURIER_HANDLE)
+                .then().log().all()
+                ;
+    }
+    public static void deleteCourier(Courier courier) {
+        int id = getCourierId(Credentials.from(courier));
+        System.out.println(id);
+        scope()
+                .body(Map.of("id", valueOf(id)))
+                .when()
+                .delete(BASIC_HANDLE + COURIER_HANDLE + "/" + id)
+                .then().log().all()
+                .statusCode(HTTP_OK)
+                .body("ok", is(true))
+                ;
+    }
+    public static void assertCreatedSuccessfully(ValidatableResponse response) {
+        response.assertThat()
+                .statusCode(HTTP_CREATED)
+                .body("ok", is(true))
+                ;
+    }
+    public static void loggedInSuccessfully(Credentials credentials) {
+        int id = loginCourier(credentials)
+                .assertThat()
+                .statusCode(HTTP_OK)
+                .extract()
+                .path("id")
+                ;
+        assert id!=0;
+    }
+    public static int getCourierId(Credentials credentials) {
+        return loginCourier(credentials)
+                .assertThat()
+                .statusCode(HTTP_OK)
+                .extract()
+                .path("id")
+                ;
     }
 }
